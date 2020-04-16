c5542d4afe0116a475a4904389a3f3372aa13f04
mathieu <math.per@hotmail.com>
2020-04-16 16:55:17 -0400

    I hate this homework

--- exam_no1.sql
+++ exam_no1.sql
@@ -1,9 +1,10 @@
 begin transaction;
 set search_path to releves_notes;
 
-CREATE VIEW notesinfo AS
-       SELECT sigle, trimc, matricule, prog, nature, note, titre
-       FROM notes NATURAL JOIN titres
+CREATE OR REPLACE VIEW notesinfo AS
+       SELECT sigle, trimc, matricule, prog, nature, note,
+       (SELECT titre FROM titres t WHERE t.sigle = n.sigle) AS titre
+       FROM notes n;
 
 CREATE TABLE log_notes (
        trimc     CHAR     (5)  NOT NULL,
@@ -15,9 +16,22 @@ CREATE TABLE log_notes (
        ts        TIMESTAMP
 );
 
-CREATE OR REPLACE FUNCTION ...
+CREATE OR REPLACE FUNCTION log_change()
+RETURNS TRIGGER AS $$
+BEGIN
+IF NEW.note != OLD.note THEN
+   insert into log_notes(trimc,sigle,matricule,prog,nature,note,ts) values
+   (OLD.trimc,OLD.sigle,OLD.matricule,OLD.prog,OLD.nature,OLD.note,now());
+END IF;
+return new;
+END;
+$$ language 'plpgsql';
 
-CREATE TRIGGER  ...
+CREATE TRIGGER  modified_note
+BEFORE UPDATE
+ON notes
+FOR EACH ROW
+EXECUTE PROCEDURE log_change();
 
 update notesinfo set note='A'
 where  sigle='IFT3911' and matricule='53357' and trimc='20114';
--- exam_no2.sql
+++ exam_no2.sql
@@ -1,8 +1,36 @@
 begin transaction;
-set search_path to releves_notes;
-create or replace function releve (...) returns table
- (...) as $$
- ...
+SET search_path TO releves_notes;
+CREATE OR REPLACE FUNCTION releve (mat char, progn char)
+RETURNS TABLE
+ (trimc     CHAR     (5) ,
+  sigle     CHAR     (10),
+  titre     VARCHAR      ,
+  cred      NUMERIC      ,
+  note      CHAR     (5)
+  ) AS $$
+WITH
+r0   as (SELECT trimc,sigle,note FROM notes
+         WHERE notes.matricule = mat AND notes.prog = progn),
+
+tab1  as (SELECT trimc,sigle,titre, credits as cred,note
+         FROM r0 NATURAL JOIN titres NATURAL JOIN credits),
+
+rz   as (SELECT trimc,sigle,cred,valeur as creds
+         FROM tab1 NATURAL JOIN valnotes),
+
+r1   as (SELECT trimc, sum(cred) AS sumcred, sum(creds*cred)/sum(cred) AS moy,
+         count(cred) AS numcred FROM rz GROUP BY trimc),
+
+mtr  as (SELECT trimc,'Moy' AS sigle, 'Moyenne du trimestre' AS titre,
+                sumcred as cred, moy as note, numcred
+         FROM r1 WHERE moy IS NOT NULL),
+cumu as (SELECT trimc,'MoyCum' AS sigle, 'Moyenne cumulative' AS titre,
+                sum(cred) OVER (ORDER BY trimc) AS cred, 'nada' AS note FROM mtr)
+SELECT * FROM tab1
+UNION ALL
+SELECT trimc,sigle, titre,cred,CAST( note AS CHAR (5)) FROM mtr
+UNION ALL
+SELECT * FROM cumu ORDER BY trimc,cred,sigle;
 $$ language 'sql';
 select * from releve('51780','117510');
 rollback;
