376902e06016b09c276f82e6d4b88a664c9c86d4
mathieu <math.per@hotmail.com>
2020-04-17 12:52:35 -0400

    finished number 3

--- exam_no3.sql
+++ exam_no3.sql
@@ -1,8 +1,25 @@
 begin transaction;
-create or replace function 
-  amort(...) returns
-  table(...) as $$
-  ...
+CREATE OR REPLACE FUNCTION
+  amort(emprunt NUMERIC, period_pay NUMERIC, taux_interet NUMERIC) RETURNS
+  TABLE(period INTEGER, pay NUMERIC, rest NUMERIC) AS $$
+
+  WITH RECURSIVE temp (period, pay, rest)
+
+  AS (SELECT 1 , 0.0 , emprunt
+      UNION
+      SELECT period+1,
+             CASE
+             WHEN ((rest +(rest*taux_interet)-period_pay) > 0)
+             THEN period_pay
+             ELSE CAST (rest+(rest*taux_interet) AS NUMERIC(100,2))
+             END,
+             CASE
+             WHEN ((rest +(rest*taux_interet)-period_pay) > 0)
+             THEN CAST (rest+(rest*taux_interet)-period_pay  AS NUMERIC(100,2) )
+             ELSE 0
+             END
+      from temp WHERE rest > 0)
+  SELECT period, pay, rest FROM temp;
 $$ language 'sql';
 select * from amort(10000.00,1000.00,0.05);
 rollback;
